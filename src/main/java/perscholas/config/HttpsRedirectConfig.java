f5465cd6807620ce60e76371dc1f48ca41a2f939
Brian <brianbrod12@gmail.com>
2024-09-01 18:12:39 -0500

    Added web.xml

---
+++ src/main/java/perscholas/config/HttpsRedirectConfig.java
@@ -0,0 +1,26 @@
+package perscholas.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsRedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return (TomcatServletWebServerFactory tomcat) -> tomcat.addAdditionalTomcatConnectors(redirectConnector());
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}
+
